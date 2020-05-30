472cfe3a3297a8618112fbcd526ade7d024df089
httpLucasReis <manoel.reisiema@gmail.com>
2020-05-30 20:10:16 -0300

    Entendendo o funcionamento da uma Promise

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 // Acesso a funcionalidade do AJAX
+
+/*
+
 var xhr = new XMLHttpRequest
 var dados = {};
 
@@ -11,4 +14,33 @@ xhr.onreadystatechange = function(){
   }
 }
 
+*/
 
+// Promise
+
+var minhaPromise = function(){
+  return new Promise(function(resolve,reject){
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://api.github.com/users/httpLucasReis');
+    xhr.send(null);
+
+    xhr.onreadystatechange = function(){
+      if(xhr.readyState == 4){
+        if(xhr.status == 200){
+           resolve(JSON.parse(xhr.responseText));
+        } else {
+           reject('Erro na requisão');
+        }
+
+      }
+    }
+ })
+}
+
+minhaPromise()
+  .then(function(response){
+      console.log(response);
+  })
+  .catch(function(error){
+      console.log(error);
+  });
